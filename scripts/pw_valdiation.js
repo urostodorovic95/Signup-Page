a1914b39025ea3adb59a0c204f3c9a86d12a4a53
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2024-02-07 00:48:39 +0100

    Add JS to validate pw match

---
+++ scripts/pw_valdiation.js
@@ -0,0 +1,26 @@
+const submitButton = document.querySelector("button");
+const userFeedback = document.querySelector("#pw-problem-notify");
+let pwConfirm = document.querySelector("input#pw-confirm");
+
+pwConfirm.addEventListener("blur", (event) => {
+  let pw = document.querySelector("input#pw");
+  if (pw.value === pwConfirm.value) {
+    processMatch();
+  } else {
+    processNoMatch();
+  }
+});
+
+function processNoMatch() {
+  userFeedback.innerHTML = "Passwords do not match.";
+  submitButton.disabled = true;
+  pwConfirm.style.border = "3px solid red";
+  pw.style.border = "3px solid red";
+}
+
+function processMatch() {
+  userFeedback.innerHTML = "";
+  submitButton.disabled = false;
+  pwConfirm.style.border = "3px solid green";
+  pw.style.border = "3px solid green";
+}
